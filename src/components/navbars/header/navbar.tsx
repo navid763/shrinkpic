eee338fcffbed0aaec9d82ce39158dc257560b6b
Navid <navidRahmati763@gmail.com>
2025-10-30 20:34:17 +0330

    edited navbar

--- src/components/navbars/header/navbar.tsx
+++ src/components/navbars/header/navbar.tsx
@@ -4,10 +4,10 @@ export default function NavBar() {
     return (
         <>
             <div className="flex flex-col text-white sm:text-neutral-800 sm:flex-row justify-center items-center gap-3">
-                <span>testOption1</span>
-                <span>test-option2</span>
-                <span>test Option3</span>
-                <span>test_ption4</span>
+                <span>COMPRESS</span>
+                <span>RESIZE</span>
+                <span>PRICING</span>
+                <span>ABOUT US</span>
             </div>
             <div className="w-full sm:w-[25%] justify-center flex gap-3">
                 <button className="w-[45%]  sm:w-[max(45%,_80px)] bg-white text-center border border-neutral-300 rounded-lg px-2.5 py-1">logIn</button>
